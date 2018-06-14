78260ed45fd1b22ec1cc8cec317bfafcfcd5b9d6
aaronreedmann <40190918+aaronreedmann@users.noreply.github.com>
2018-06-14 08:49:03 -0500

    create script_types
    this file simply houses some script types that we check for and remove in our form scripting

---
+++ script_types.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+//- scripting languages array
+//- see: http://en.wikipedia.org/wiki/List_of_programming_languages
+
+//- this file is provided to help prevent XSS attacks
+//- people could use a lot of different languages for XSS attack
+//- another filtering method exists but it is very inefficient
+
+//- this array could be used for other purposes than XSS prevention.
+*/
+
+$Def_ScriptTypes = array(
+	'javascript', 
+	'javafxscript', 
+	'javafx', 
+	'ecmascript', 
+	'vbscript', 
+	'fscript', 
+	'f-script', 
+	'jscript', 
+	'actionscript', 
+	'microscript', 
+	'newtonscript', 
+	'postscript', 
+	'qtscript', 
+	'rbscript', 
+	'shiftscript', 
+	'redcode', 
+	'ubercode', 
+	'unrealscript', 
+);
+
+?>
