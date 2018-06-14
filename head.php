b41fd0b314a0e3f21cb37f8abb2a507d868f5630
aaronreedmann <40190918+aaronreedmann@users.noreply.github.com>
2018-06-14 08:56:25 -0500

    created head.php
    this file holds code that should be put in the HTML page headers

---
+++ head.php
@@ -0,0 +1,14 @@
+<?php
+
+//- - - - - - - - - - - - - - - - - - - - - - - -
+//- Title: Contact Form Header Settings
+//- Description: settings that need to be included in the page header
+//- Author: Aaron Mann
+//- Updated: 12 June 2018
+
+
+echo '<link rel="stylesheet" type="text/css" media="all" href="style.css">'.$n;
+
+
+
+?>
